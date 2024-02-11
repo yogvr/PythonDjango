6f7f3a46eb8ecb7fd6fef39922b9545578d49416
yogvr <laura.bv9@gmail.com>
2024-02-11 15:50:18 +0100

    tema1

---
+++ Tema 1/Proyecto 1 LBV.py
@@ -0,0 +1,3 @@
+# Vas a crear un código que le pida al usuario que responda dos preguntas que requieran una sola palabra cada una, y que luego le muestre en pantalla esas palabras combinadas para formar una marca.
+
+print("Tu marca es:A\n", input("¿Cuál es tu animal favorito?:\n"), input("¿De qué color es tu cepillo de dientes?\n"))
